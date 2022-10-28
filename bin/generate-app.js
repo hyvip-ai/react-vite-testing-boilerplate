e40cc09d855daabc42ec76dafccf38c1be4fb708
rajatcbd <rm2932002@gmail.com>
2022-10-29 02:02:06 +0530

    Removed lock files

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -3,11 +3,13 @@
 const { execSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
+console.log(chalk.blue('Creating your boilerplate'));
 
 if (process.argv.length < 3) {
-  console.log('You have to provide a name to your app.');
-  console.log('For example :');
-  console.log('    npx create-my-boilerplate my-app');
+  console.log(chalk.green('You have to provide a name to your app.'));
+  console.log(chalk.green('For example :'));
+  console.log(chalk.green('    npx create-my-boilerplate my-app'));
   process.exit(1);
 }
 
@@ -22,29 +24,29 @@ try {
 } catch (err) {
   if (err.code === 'EEXIST') {
     console.log(
-      `The file ${projectName} already exist in the current directory, please give it another name.`
+      error(
+        `The directory "${projectName}" already exist in the current directory, please give it another name.`
+      )
     );
   } else {
-    console.log(error);
+    console.log(error(err));
   }
   process.exit(1);
 }
 
 async function main() {
   try {
-    console.log('Downloading files...');
+    console.log(chalk.green('Downloading files...'));
+    console.log(chalk.green('Cloning repository...'));
     execSync(`git clone --depth 1 ${git_repo} ${projectName}`);
-
     process.chdir(projectPath);
-
-    console.log('Installing dependencies...');
-    execSync('npm install');
-
-    console.log('Removing useless files');
+    console.log(chalk.green('Removing useless files'));
     execSync('npx rimraf ./.git');
-    fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true });
-
-    console.log('The installation is done, this is ready to use !');
+    fs.rmSync(path.join(projectPath, 'bin'), { recursive: true });
+    console.log(`${chalk.green('Done. Now Run:')}`);
+    console.log(`      ${chalk.bold.yellow(`cd ${projectName}`)}`);
+    console.log(`      ${chalk.bold.yellow(`yarn`)}`);
+    console.log(`      ${chalk.bold.yellow(`yarn dev || yarn test`)}`);
   } catch (error) {
     console.log(error);
   }
